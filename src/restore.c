140b854a678d213b151178259671238334c18492
PrimePlatypus <53546967+PrimePlatypus@users.noreply.github.com>
2023-02-23 19:38:32 -0500

    Update restore.c

--- src/restore.c
+++ src/restore.c
@@ -855,7 +855,6 @@ static int restore_handle_bb_update_status_msg(restored_client_t client, plist_t
 
 	if (!accepted) {
 		error("ERROR: device didn't accept BasebandData\n");
-		return result;
 	}
 
 	uint8_t done = 0;
